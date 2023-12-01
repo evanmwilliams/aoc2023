58fbbc068bb06d8aac71454b12fa6d93f257d8d4
Evan Williams <emw236@cornell.edu>
2023-12-01 16:45:21 -0500

    clean up part 1

--- day1/part1.cpp
+++ day1/part1.cpp
@@ -1,4 +1,3 @@
-#include <numeric>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -14,14 +13,11 @@ int main(int argc, char *argv[])
   while (std::getline(file, line))
   {
     std::vector<int> digits;
-    for (int i = 0; i < line.size(); i++)
-    {
-      const auto c = line[i];
-      if (c >= '0' && c <= '9')
-      {
-        digits.push_back(c - '0');
-      }
-    }
+	for (auto c: line) {
+	  if (isdigit(c)) {
+		digits.push_back(c - '0');
+	  }
+	}
     calibration_sum += digits[0] * 10 + digits.back();
   }
 
